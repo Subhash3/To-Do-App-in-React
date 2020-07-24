1bb8cd015bd1e1d8f844bb43869c35b98b72798c
Subhash Sarangi <subhashsarangi123@gmail.com>
2020-07-24 11:48:08 +0530

    Updates

---
+++ precache-manifest.aaea0d5c18a04c7f3692a4279e6b76dc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0511facc68b80f4723e23ec6df605d15",
+    "url": "/To-Do-App-in-React/index.html"
+  },
+  {
+    "revision": "33db02ddfdef261c05b9",
+    "url": "/To-Do-App-in-React/static/css/2.0ac29b89.chunk.css"
+  },
+  {
+    "revision": "262eeed08eec1f8fba2c",
+    "url": "/To-Do-App-in-React/static/css/main.031e6795.chunk.css"
+  },
+  {
+    "revision": "33db02ddfdef261c05b9",
+    "url": "/To-Do-App-in-React/static/js/2.f23755c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-App-in-React/static/js/2.f23755c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "262eeed08eec1f8fba2c",
+    "url": "/To-Do-App-in-React/static/js/main.36657c99.chunk.js"
+  },
+  {
+    "revision": "e40a7c99d919d4e7f734",
+    "url": "/To-Do-App-in-React/static/js/runtime-main.d9fa282d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/To-Do-App-in-React/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/To-Do-App-in-React/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/To-Do-App-in-React/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/To-Do-App-in-React/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/To-Do-App-in-React/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
